159349850cc8d2677f7419fea86caf586ead0a5f
AnnaVyvert <0073av@gmail.com>
2023-07-28 12:22:14 +0800

    feat: lap store worker

---
+++ src/workers/lap-store-worker.js
@@ -0,0 +1,26 @@
+function findMaxIdInLapStore() {
+  let store = preprocessLapStore();
+  if (!store) return 0;
+
+  const ids = store.map((e) => e.id);
+  return Math.max(...ids);
+}
+
+function getSumTimeInLapStore() {
+  let store = lapStoreCRUD().read()
+  if (!store) return 0;
+  
+  return store.reduce((acc, el) => {
+    return !el.disabled ? acc + el.time : acc + 0;
+  }, 0);
+}
+
+function preprocessLapStore() {
+  let store = getValueFromStore(LAPS_STORE_NAME);
+  if (!store) return 0;
+  store = JSON.parse(store);
+
+  if (!store || !store.length) return 0;
+
+  return store;
+}

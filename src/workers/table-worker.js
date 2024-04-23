91bc8ec3bdadfecd4a692f100aa6390ba952b086
AnnaVyvert <0073av@gmail.com>
2024-04-23 20:47:45 +0800

    fix: 🐛 input titles, optimize note focusouts, support links

--- src/workers/table-worker.js
+++ src/workers/table-worker.js
@@ -1,18 +1,39 @@
 const ROW_ID = 'row_';
 
+function findAndProcessLinks(inputText) {
+  const replacePattern1 = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/g;
+  let replacedText = inputText.replace(replacePattern1, function(match) {
+    const lastIndex = Math.max(match.lastIndexOf("/"), match.lastIndexOf("="));
+    const linkText = match.slice(lastIndex+1);
+    return '<a target="_blank" href="' + match + '">' + linkText + '</a>';
+  });
+
+  const replacePattern2 = /(^|[^\/])(www\.[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?)/g;
+  replacedText = replacedText.replace(replacePattern2, function(match, group1, group2) {
+    const lastIndex = Math.max(group2.lastIndexOf("/"), group2.lastIndexOf("="));
+    const linkText = group2.slice(lastIndex+1);
+    return group1 + '<a target="_blank" href="http://' + group2 + '">' + linkText + '</a>';
+  });
+
+  return replacedText;
+}
+
 function editCell(rowId, id, key) {
   if (getRefAction(rowId).isLocked) return;
   const spanRef = document.querySelector(`tr#${ROW_ID}${rowId} span[id="${id}"]`);
   const inputRef = document.querySelector(`tr#${ROW_ID}${rowId} input[id="${id}"]`);
   if (spanRef.textContent && spanRef.textContent !== NOTE_CONTENT_EMPTY) {
-    inputRef.value = spanRef.textContent.trim();
+    inputRef.value = getValueFromLapStore(Number(rowId))[key];
   }
   spanRef.hidden = !spanRef.hidden;
   inputRef.hidden = !inputRef.hidden;
+
+  if (inputRef.getAttribute('data-hasListener')) return;
   inputRef.addEventListener('focusout', (ev) => {
     if (key === 'note') {
-      spanRef.textContent = inputRef.value.trim() || NOTE_CONTENT_EMPTY;
-      updateValueInLapStore(Number(rowId), key, inputRef.value.trim() || NOTE_CONTENT_EMPTY);
+      const _spanValue = findAndProcessLinks(inputRef.value.trim()) || NOTE_CONTENT_EMPTY;
+      spanRef.innerHTML = _spanValue;
+      updateValueInLapStore(Number(rowId), key, inputRef.value || NOTE_CONTENT_EMPTY);
       inputRef.hidden = true;
       spanRef.hidden = false;
     } else if (key === 'time') {
@@ -30,6 +51,7 @@ function editCell(rowId, id, key) {
       spanRef.hidden = false;
     }
   });
+  inputRef.setAttribute('data-hasListener', true);
 }
 
 function tableAddRow({ tableRef, isRowDisabled, rowId, data, editableData }) {
@@ -45,8 +67,8 @@ function tableAddRow({ tableRef, isRowDisabled, rowId, data, editableData }) {
       `;
     } else if (el.key === 'note') {
       data[ind] = `
-        <input id=${i + 1} class="${el.key} center" hidden style="width: ${el.width}px" title="follow hh:mm:ss format">
-        <span id=${i + 1} class="${el.key}" onclick="editCell(${rowId}, ${i + 1}, '${el.key}')" title="click to edit time">
+        <input id=${i + 1} class="${el.key} center" hidden style="width: ${el.width}px" title="you can also add links here">
+        <span id=${i + 1} class="${el.key}" onclick="editCell(${rowId}, ${i + 1}, '${el.key}')" title="click to edit note">
           ${el.data}
         </span>
       `;
@@ -63,6 +85,7 @@ function tableAddRow({ tableRef, isRowDisabled, rowId, data, editableData }) {
     </tr>
   `
   tableRef.innerHTML = tbodySplitted[0] + newRow + tbodySplitted[1];
+  document.querySelector(`tr[id="${ROW_ID}${rowId}"] span.note`).innerHTML = findAndProcessLinks(editableData[1].data);
 }
 
 function updateLapsTableVisibility() {
